6378cd71a2757a6188a8bef97fc4646ec93298e1
Just1dbr <debruynejustin9@gmail.com>
2023-02-09 15:21:52 +0100

    mise en page

--- inscription.js
+++ inscription.js
@@ -1,23 +1,23 @@
-const formulaire=document.querySelector("form")
+const formulaire = document.querySelector("form")
 
-function envoyerFormulaire (e) {
+function envoyerFormulaire(e) {
     e.preventDefault();
     const target = e.target
 
     const email = target.querySelector('#email').value
-const motdepasse = target.querySelector('#password').value
-const motdepasse2=target.querySelector('#password2').value
-if(motdepasse === motdepasse2) {
-    document.querySelector('.title').style.display = 'none';
-    document.querySelector('.imageetform').style.display = 'none';
-    
-    document.querySelector('.success').style.display = 'block';
-    document.querySelector('.success').innerHTML = 'Merci pour votre inscription ! Vous allez recevoir un email de confirmation à ' + email
-    return false;
+    const motdepasse = target.querySelector('#password').value
+    const motdepasse2 = target.querySelector('#password2').value
 
-} else {
-    alert("erreur")
-}
+    if (motdepasse === motdepasse2) {
+        document.querySelector('.title').style.display = 'none';
+        document.querySelector('.imageetform').style.display = 'none';
+
+        document.querySelector('.success').style.display = 'block';
+        document.querySelector('.success').innerHTML = 'Merci pour votre inscription ! Vous allez recevoir un email de confirmation à ' + email
+        return false;
+    } else {
+        alert("erreur")
+    }
 }
 
 formulaire.addEventListener('submit', envoyerFormulaire, false)
